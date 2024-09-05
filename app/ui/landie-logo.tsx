dabd64b652800b3726be71837f3b7bce0dca488e
bagoviggo <bagoviggo@gmail.com>
2024-09-05 23:34:20 +0300

    :sparkles: Add dashboard pages

--- app/ui/landie-logo.tsx
+++ app/ui/landie-logo.tsx
@@ -5,9 +5,10 @@ export default function LandieLogo() {
     <div className="flex shrink-0 items-end rounded-lg p-4">
       <Image
         src="/logo.png"
-        width={160}
-        height={160}
-        className="w-32 md:w- h-auto"
+        width={200} // Increase the width to 300
+        height={200
+        } // Increase the height to 300
+        className="w-48 h-auto" // Increase the width to 48
         alt="Landie Logo"
         priority={true}
       />

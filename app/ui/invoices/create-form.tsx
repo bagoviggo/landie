237cd914d4004c47c5b26bae9f4e5919327084a4
bagoviggo <bagoviggo@gmail.com>
2024-09-06 13:39:02 +0300

    :fire: fix name error

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,4 +1,4 @@
-import { CustomerField } from '@/app/lib/definitions';
+import { TenantField } from '@/app/lib/definitions';
 import Link from 'next/link';
 import {
   CheckIcon,
@@ -8,28 +8,28 @@ import {
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
 
-export default function Form({ customers }: { customers: CustomerField[] }) {
+export default function Form({ tenants }: { tenants: TenantField[] }) {
   return (
     <form>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
-        {/* Customer Name */}
+        {/* Tenant Name */}
         <div className="mb-4">
-          <label htmlFor="customer" className="mb-2 block text-sm font-medium">
-            Choose customer
+          <label htmlFor="tenant" className="mb-2 block text-sm font-medium">
+            Choose tenant
           </label>
           <div className="relative">
             <select
-              id="customer"
-              name="customerId"
+              id="tenant"
+              name="tenantId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
             >
               <option value="" disabled>
-                Select a customer
+                Select a tenant
               </option>
-              {customers.map((customer) => (
-                <option key={customer.id} value={customer.id}>
-                  {customer.name}
+              {tenants.map((tenant) => (
+                <option key={tenant.id} value={tenant.id}>
+                  {tenant.name}
                 </option>
               ))}
             </select>

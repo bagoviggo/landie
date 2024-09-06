d31e3bdb7c144659c22b5c1f274b9f53392e1744
bagoviggo <bagoviggo@gmail.com>
2024-09-06 15:27:56 +0300

    :wrench: changed node version to 20.17.0 lts

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -5,6 +5,7 @@ const users = [
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
     name: 'User',
     email: 'user@nextmail.com',
+    phone: '+254 712 345 678',
     password: '123456',
   },
 ];
@@ -14,36 +15,42 @@ const customers = [
     id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
     name: 'Evil Rabbit',
     email: 'evil@rabbit.com',
+    phone: '+254 712 345 679',
     image_url: '/customers/evil-rabbit.png',
   },
   {
     id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
     name: 'Delba de Oliveira',
     email: 'delba@oliveira.com',
+    phone: '+254 712 345 680',
     image_url: '/customers/delba-de-oliveira.png',
   },
   {
     id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
     name: 'Lee Robinson',
     email: 'lee@robinson.com',
+    phone: '+254 712 345 681',
     image_url: '/customers/lee-robinson.png',
   },
   {
     id: '76d65c26-f784-44a2-ac19-586678f7c2f2',
     name: 'Michael Novotny',
     email: 'michael@novotny.com',
+    phone: '+254 712 345 682',
     image_url: '/customers/michael-novotny.png',
   },
   {
     id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144B9',
     name: 'Amy Burns',
     email: 'amy@burns.com',
+    phone: '+254 712 345 683',
     image_url: '/customers/amy-burns.png',
   },
   {
     id: '13D07535-C59E-4157-A011-F8D2EF4E0CBB',
     name: 'Balazs Orban',
     email: 'balazs@orban.com',
+    phone: '+254 712 345 684',
     image_url: '/customers/balazs-orban.png',
   },
 ];
@@ -70,7 +77,7 @@ const invoices = [
   {
     customer_id: customers[3].id,
     amount: 44800,
-    status: 'paid',
+    status: 'late',
     date: '2023-09-10',
   },
   {

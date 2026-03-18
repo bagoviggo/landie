3777e8a12e6f39395fdb1c23d70f8521af4ba2eb
bagoviggo <bagoviggo@gmail.com>
2026-03-18 14:18:20 +0300

    Fix: New Landing page Build Error

--- app/context/auth-provider.tsx
+++ app/context/auth-provider.tsx
@@ -3,18 +3,7 @@
 import { SessionProvider } from 'next-auth/react';
 import { ReactNode } from 'react';
 
-export functio'use client';
-
-import { SessionProvider } from 'next-auth/react';
-import { ReactNode } from 'react';
-
 export function AuthProvider({ children }: { children: ReactNode }) {
-  return (
-    <SessionProvider refetchInterval={0} refetchOnWindowFocus={false}>
-      {children}
-    </SessionProvider>
-  );
-}n AuthProvider({ children }: { children: ReactNode }) {
   return (
     <SessionProvider refetchInterval={0} refetchOnWindowFocus={false}>
       {children}
